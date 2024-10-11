5388aa5fcbc24b6765dc6101a1e845ae4518e277
Artos1Tash <strong.willed343@gmail.com>
2024-10-10 23:41:49 -0400

    Tryna deploy

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,7 @@
+/** @type {import('next').NextConfig} */
 const nextConfig = {
-    output: 'export',
-  };
-  export default nextConfig;
+  output: 'export',
+  // Add this if you're deploying to a subdirectory (like GitHub Pages project sites)
+  // basePath: '/your-repo-name',
+};
+export default nextConfig;
