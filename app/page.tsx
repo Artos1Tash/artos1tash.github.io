e6f7fb6e4cfa849f1177442c75f71d53ee48e0b3
Artos1Tash <strong.willed343@gmail.com>
2024-10-14 18:16:57 -0400

    Fixing images

--- app/page.tsx
+++ app/page.tsx
@@ -1,22 +1,22 @@
 import Navbar from "@/components/Navbar";
-import Image from "next/image";
+// import Image from "next/image";
 
 export default function Home() {
   return (
     <>
-      <div className="relative w-[100%] h-[100vh] mx-auto overflow-hidden bg-[url('/nextjs-github-pages/images/city2.jpg')] bg-cover bg-center">
+      <div className="relative w-[100%] h-[100vh] mx-auto overflow-hidden bg-[url('/images/city2.jpg')] bg-cover bg-center">
         <div className="w-full h-full bg-black/50">
           <div className="text-center p-10 flex justify-center text-xl text-white">
             <Navbar></Navbar>
           </div>
         </div>
       </div>
-      <section className="flex flex-col  bg-gray-200">
+      {/* <section className="flex flex-col  bg-gray-200">
 
         <div className="max-w-6xl mx-auto mt-64 mb-64 grid md:grid-cols-2 gap-8 items-center">
           <div className="relative w-full h-full pb-[100%] md:pb-[75%] rounded-2xl overflow-hidden shadow-lg">
             <Image
-              src="/nextjs-github-pages/images/rocks.jpg"
+              src="/images/rocks.jpg"
               alt="Artur"
               fill
               className="object-cover"
@@ -36,7 +36,7 @@ export default function Home() {
             </p>
           </div>
         </div>
-      </section>
+      </section> */}
 
     </>
 
